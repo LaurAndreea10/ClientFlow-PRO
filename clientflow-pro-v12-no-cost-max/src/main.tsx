b80f5b04071b219a466fc66ce258beb9f1cd51ff
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-04-23 14:24:41 +0300

    Use HashRouter across app variants to avoid static-host white page

--- clientflow-pro-v12-no-cost-max/src/main.tsx
+++ clientflow-pro-v12-no-cost-max/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
@@ -25,9 +25,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ThemeProvider>
         <LocaleProvider>
           <ToastProvider>
-            <BrowserRouter>
+            <HashRouter>
               <App />
-            </BrowserRouter>
+            </HashRouter>
           </ToastProvider>
         </LocaleProvider>
       </ThemeProvider>
