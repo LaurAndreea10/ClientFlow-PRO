43f34128d1a6214577e6c0b0af0a1566423f4891
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-04-24 19:44:09 +0300

    Wire functional suite modules into routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import { ActivityPage } from './pages/ActivityPage'
 import { CalendarPage } from './pages/CalendarPage'
 import { CaseStudyPage } from './pages/CaseStudyPage'
 import { ClientDetailsPage } from './pages/ClientDetailsPage'
+import { ClientPortalPage } from './pages/ClientPortalPage'
 import { ClientsPage } from './pages/ClientsPage'
 import { DashboardPage } from './pages/DashboardPage'
+import { DemoPlannerPage } from './pages/DemoPlannerPage'
+import { ImpactGoalsPage } from './pages/ImpactGoalsPage'
+import { InvoicingPage } from './pages/InvoicingPage'
 import { LoginPage } from './pages/LoginPage'
 import { NotFoundPage } from './pages/NotFoundPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { ReportsPage } from './pages/ReportsPage'
+import { ServicesPage } from './pages/ServicesPage'
 import { SettingsPage } from './pages/SettingsPage'
 import { SuitePage } from './pages/SuitePage'
 import { TasksPage } from './pages/TasksPage'
+import { TimeTrackingPage } from './pages/TimeTrackingPage'
 import { ProtectedRoute } from './routes/ProtectedRoute'
 
 export default function App() {
@@ -29,6 +35,12 @@ export default function App() {
           <Route path="/clients" element={<ClientsPage />} />
           <Route path="/clients/:id" element={<ClientDetailsPage />} />
           <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/invoices" element={<InvoicingPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/time" element={<TimeTrackingPage />} />
+          <Route path="/portal" element={<ClientPortalPage />} />
+          <Route path="/demo-planner" element={<DemoPlannerPage />} />
+          <Route path="/impact" element={<ImpactGoalsPage />} />
           <Route path="/reports" element={<ReportsPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/activity" element={<ActivityPage />} />
